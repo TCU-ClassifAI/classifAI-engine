7f300315a00e843f53865f2348dfc6dbac8b92fe
John Mejia <29128358+jhmejia@users.noreply.github.com>
2023-11-21 19:40:44 -0600

    Add environment files

--- src/run.py
+++ src/run.py
@@ -1,8 +1,6 @@
 from flask import Flask, make_response
 import os
 
-# from services.transcription import transcribe, start_transcription, check_transcription
-
 from services.transcription.profile import profile
 from services.transcription.views import transcription
 
@@ -11,54 +9,6 @@ if os.environ.get("ENV") == "production":
     from config import production as settings
 else:
     from config import development as settings
-
-
-# @app.route("/start_transcription", methods=["POST"])
-# def start_transcription_endpoint():
-#     # Check if the file was uploaded
-#     if "file" not in request.files:
-#         return make_response("No file uploaded", 400)
-
-#     file = request.files["file"]
-
-#     # print the file name
-#     print(file.filename)
-
-#     model_type = (
-#         request.form["model_type"]
-#         if "model_type" in request.form
-#         else config.MODEL_TYPE
-#     )
-
-#     job_id = start_transcription(file, model_type)
-
-#     return make_response(job_id, 200)
-
-
-# @app.route("/get_transcription", methods=["GET"])
-# def get_transcription_endpoint():
-#     # Get the query parameters
-#     job_id = request.form.get("job_id", default=None)
-
-#     print(job_id)
-
-#     try:
-#         result = check_transcription(job_id)
-#         return make_response(result, 200)
-#     except Exception as e:
-#         return make_response(str(e), 500)
-
-
-# @app.route("/transcription", methods=["GET", "POST"])
-# def get_transcription():
-#     # Get the query parameters
-#     path = request.args.get("path", default=None)
-
-#     try:
-#         result = transcribe(path)
-#         return make_response(result, 200)
-#     except Exception as e:
-#         return make_response(str(e), 500)
 
 app = Flask(__name__)
 app.register_blueprint(profile, url_prefix="/profile")
@@ -81,6 +31,5 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    local = True
-
+    local = True if os.environ.get("ENV") == "development" else False
     app.run(debug=True)
