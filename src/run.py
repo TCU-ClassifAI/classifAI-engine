4832f4fd4941d747fa682df94cc301ea3a188201
John Mejia <29128358+jhmejia@users.noreply.github.com>
2023-11-24 16:49:27 -0600

    Update index url

--- src/run.py
+++ src/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, redirect
 import os
 from dotenv import load_dotenv
 from services.transcription.profile import profile
@@ -16,6 +16,28 @@ else:
 app = Flask(__name__)
 app.register_blueprint(profile, url_prefix="/profile")
 app.register_blueprint(transcription, url_prefix="/transcription")
+
+
+@app.route("/help", methods=["GET"])
+def help():
+    """Forwards to the documentation page (https://tcu-classifai.github.io/classifAI-engine/)"""
+
+    return redirect("https://tcu-classifai.github.io/classifAI-engine/", code=302)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """Gives a brief description of the API, version, config, and healthcheck. Welcome page"""
+
+    description = "ClassifAI Engine"
+    version = "1.0.0"
+    config = settings.SETTINGS_TYPE
+    healthcheck = "OK"
+    documentation = "https://tcu-classifai.github.io/classifAI-engine/"
+
+    return "<h1>{}</h1><p>Version: {}</p><p>Config: {}</p><p>Healthcheck: {}</p><a href='{}'>Documentation</a>".format(
+        description, version, config, healthcheck, documentation
+    )
 
 
 @app.route("/healthcheck", methods=["GET"])
