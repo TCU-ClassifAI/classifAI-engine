0f14fb0f9ec8eb533ae1f42154bc0696b42c93f4
azofurry <29128358+jhmejia@users.noreply.github.com>
2024-04-17 23:22:27 -0500

    Update config with allowed extensions and upload folder

--- src/config/config.py
+++ src/config/config.py
@@ -8,8 +8,28 @@ TRANSCRIPTION_MODEL = "large-v3"
 CATEGORIZATION_MODEL = "gemma"  # or gpt
 SUMMARIZATION_MODEL = "huggingface"  # or gpt
 ENV_TYPE = "dev"
+
+# File upload settings
+
+# Audio file upload settings
 UPLOAD_FOLDER = "raw_audio/"
 
+ALLOWED_EXTENSIONS = {
+    "wav",
+    "mp3",
+    "mp4",
+    "m4a",
+    "flac",
+    "aac",
+    "wma",
+    "ogg",
+    "oga",
+    "webm",
+    "mov",
+} # Others are probably supported as well but not tested.
+
+# Includes vocal separation outputs and diarization rich transcription time marked (rttm) files
+TEMP_FOLDER = "temp_outputs/"  
 
 if os.environ.get("ENV") == "production":
     ENV_TYPE = "prod"
