78cd6b7193316a8649aed5c2e892c8bce7d5c63e
azofurry <29128358+jhmejia@users.noreply.github.com>
2024-05-05 11:51:11 -0500

    Update llama

--- src/utils/categorize/categorize_llama.py
+++ src/utils/categorize/categorize_llama.py
@@ -3,18 +3,19 @@ from dotenv import load_dotenv
 import os
 import random as rand
 
+
 load_dotenv()
 
 
 def categorize_question(question: str) -> int:
     """
-    Categorize the question using GEMMA
+    Categorize the question using LLAMA
     """
     # call GEMMA API
 
     try:
         response = requests.post(
-            f"{os.getenv('GEMMA_API_URL')}/categorize", json={"question": question}
+            f"{os.getenv('LLAMA_API_URL')}/categorize", json={"question": question}
         )
 
     except Exception as e:
@@ -22,7 +23,7 @@ def categorize_question(question: str) -> int:
         return rand.randint(0, 3)
     
 
-    print(response.json())
+    print(f"Response for question: {question} is {response.json().get('response')}")
 
     # return the category
     return int(response.json().get("response"))
