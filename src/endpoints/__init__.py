46ca2bcc58b322fe7899f9f7574ba25a7b4004bd
azofurry <29128358+jhmejia@users.noreply.github.com>
2024-05-06 18:20:13 -0500

    revert __init__ imports with suggested __all__ workaround for flake8

---
+++ src/endpoints/__init__.py
@@ -0,0 +1,7 @@
+from .categorize import categorize 
+from .summarize import summarize
+from .transcription import transcription
+from .analyze import analyze
+from .server_info import server_info
+
+__all__ = ['categorize', 'summarize', 'transcription', 'analyze', 'server_info']
